1685ee9c2a13ce9e22d8c498efbeb8c6a0d6b7b8
cryptology-nodes <107956881+cryptology-nodes@users.noreply.github.com>
2022-07-11 15:50:00 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,82 +1,72 @@
 #!/bin/bash
-cd $HOME
-if [ ! $OWNER ]; then
-	read -p "Ваш Discord Username без #: " OWNER
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
-echo 'Владелец: ' $OWNER
-sleep 1
-echo 'export OWNER='$OWNER >> $HOME/.profile
-if [ ! $HOSTNAME ]; then
-	read -p "Введите название своего сервера: " HOSTNAME
+
+sleep 1 && curl -s https://raw.githubusercontent.com/cryptology-nodes/main/main/logo.sh |  bash && sleep 2
+
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
 fi
-echo 'Название вашего сервера: ' $HOSTNAME
-sleep 1
-echo 'export HOSTNAME='$HOSTNAME >> $HOME/.profile
 
-sudo systemctl stop prometheus && systemctl disable prometheus
+# update and install new packages
+sudo apt update
+sudo apt install \
+    ca-certificates \
+    gnupg \
+    lsb-release
+# add docker gpg keys
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#add docker stable repo
 
-sudo mkdir /etc/prometheus
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt install wget -y
-wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.63.0/vmutils-amd64-v1.63.0.tar.gz
-tar xvf vmutils-amd64-v1.63.0.tar.gz
-rm -rf vmutils-amd64-v1.63.0.tar.gz
+#update and install docker
 
-sudo tee <<EOF >/dev/null /etc/prometheus/prometheus.yml
-global:
-  scrape_interval: 30s
-  evaluation_interval: 30s
-  external_labels:
-    owner: '$OWNER'
-    hostname: '$HOSTNAME'
-scrape_configs:
-  - job_name: "node_exporter"
-    scrape_interval: 30s
-    static_configs:
-      - targets: ["localhost:9100"]
-    relabel_configs:
-      - source_labels: [__address__]
-        regex: '.*'
-        target_label: instance
-        replacement: '$HOSTNAME'
-EOF
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+apt  install docker-compose
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
-[Unit]
-  Description=vmagent Monitoring
-  Wants=network-online.target
-  After=network-online.target
-[Service]
-  User=$USER
-  Type=simple
-  ExecStart=$HOME/vmagent-prod \
-  -promscrape.config=/etc/prometheus/prometheus.yml \
-  -remoteWrite.url=http://cryptology:cryptology@cryptologynodes.grafana.net:8080/api/v1/write
-  ExecReload=/bin/kill -HUP $MAINPID
-[Install]
-  WantedBy=multi-user.target
-EOF
+#install node exporter
+touch docker-compose.yml
+echo "
+version: '3.3'
 
-sudo systemctl daemon-reload && sudo systemctl enable vmagent && sudo systemctl restart vmagent
+networks:
+  monitoring:
+    driver: bridge
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
-tar xvf node_exporter-1.1.2.linux-amd64.tar.gz
-sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
-rm -rf node_exporter-1.1.2.linux-amd64*
+volumes:
+  prometheus_data: {}
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/node_exporter.service
-[Unit]
-Description=Node Exporter
-Wants=network-online.target
-After=network-online.target
-[Service]
-User=$USER
-Type=simple
-ExecStart=/usr/local/bin/node_exporter --web.listen-address=":9100"
-[Install]
-WantedBy=multi-user.target
-EOF
+services:
+  node-exporter:
+    image: prom/node-exporter:latest
+    container_name: node-exporter
+    restart: unless-stopped
+    volumes:
+      - /proc:/host/proc:ro
+      - /sys:/host/sys:ro
+      - /:/rootfs:ro
+    command:
+      - '--path.procfs=/host/proc'
+      - '--path.rootfs=/rootfs'
+      - '--path.sysfs=/host/sys'
+      - '--collector.filesystem.mount-points-exclude=^/(sys|proc|dev|host|etc)($$|/)'
+    ports:
+      - 9100:9100
+    networks:
+      - monitoring"
+      >> docker-compose.yml
+#start docker image
+docker-compose up -d
 
-sudo systemctl daemon-reload && sudo systemctl enable node_exporter && sudo systemctl restart node_exporter
-
-echo "Monitoring Installed"
